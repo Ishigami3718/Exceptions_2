9034c33b1775bd97554f16723f79d5594d9ddc2b
Leschenko_Rostyslav <rostishkaleschenko2006@gmail.com>
2024-11-17 17:26:27 +0200

    start

--- Exceptions2/Program.cs
+++ Exceptions2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Directory.GetFiles(Directory.GetCurrentDirectory());
+            Regex regexExtForImage = new Regex("^((bmp)|(gif)|(tiff?)|(jpe?g)|(png))$", RegexOptions.IgnoreCase);,
+            foreach (string file in data)
+            {
+                try
+                {
+                    Bitmap bitmap = new Bitmap(file);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
