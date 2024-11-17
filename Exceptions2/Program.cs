7554a3dffab5b56fddf07b57bab7ef52514ba0cc
Leschenko_Rostyslav <rostishkaleschenko2006@gmail.com>
2024-11-17 18:30:22 +0200

    mirorring and save file

--- Exceptions2/Program.cs
+++ Exceptions2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.Text.RegularExpressions;
+using System.Windows;
 
 namespace Lab
 {
@@ -9,16 +10,20 @@ namespace Lab
         static void Main(string[] args)
         {
             string[] data = Directory.GetFiles(Directory.GetCurrentDirectory());
-            Regex regexExtForImage = new Regex("^((bmp)|(gif)|(tiff?)|(jpe?g)|(png))$", RegexOptions.IgnoreCase);,
+            Regex regexExtForImage = new Regex("^((bmp)|(gif)|(tiff?)|(jpe?g)|(png))$", RegexOptions.IgnoreCase);
             foreach (string file in data)
             {
                 try
                 {
                     Bitmap bitmap = new Bitmap(file);
+                    Bitmap mirrored = (Bitmap)bitmap.Clone();
+                    mirrored.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                    string newfile = new Regex(".[a-b]").Replace(file, string.Empty) + "-mirrored.gif";
+                    mirrored.Save(newfile);
                 }
                 catch
                 {
-
+                    if (regexExtForImage.IsMatch(Path.GetExtension(file))) Console.WriteLine("Файл не був прочитаний, хоча мав би бути");
                 }
             }
         }
